7c8ec861f966ca80579b6ed1e6a2bd5d5271b860
bbenjii <benji.ollomo@gmail.com>
2025-04-13 22:37:48 -0400

    updates

--- src/auth.js
+++ src/auth.js
@@ -1,3 +1,6 @@
+export const runtime = "nodejs";
+export const dynamic = 'force-dynamic';
+
 import NextAuth from "next-auth"
 // import { ZodError } from "zod"
 import Credentials from "next-auth/providers/credentials"
